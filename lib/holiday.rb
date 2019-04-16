d88af753aabade4ea9340a0d2d76fbc978f4451d
Stella McEwen <mcewen.stella@gmail.com>
2019-04-16 22:45:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |keys, values|
-   "#{key}: #{value}"
+   return "#{key}: #{value}"
 end
 end
 
