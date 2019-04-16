56730f427ba1e3944a0b52239c9e580cc3062785
Stella McEwen <mcewen.stella@gmail.com>
2019-04-16 23:16:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_hash.collect do |seasons, holiday|
+holiday_hash.collect do |seasons, holidays|
   
 end
 
