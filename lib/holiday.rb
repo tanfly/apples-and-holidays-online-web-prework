876dd328864aa7c26d742bfe4f5a02ffe3e28b71
Stella McEwen <mcewen.stella@gmail.com>
2019-04-16 23:18:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_holidays_with_bbq(holiday_hash)
 holiday_hash.collect do |seasons, holidays|
   holidays.collect do |holiday, supplies|
     if supplies.include?("BBQ")
-      holiday
+      holiday.flatten
   
 end
 
