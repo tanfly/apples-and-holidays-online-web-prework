31a3bf829b0bb9b0ce38ea8e4de59ec0e8dacdc9
Stella McEwen <mcewen.stella@gmail.com>
2019-04-16 22:39:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,8 +34,7 @@ holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
+  holiday_hash
 
 end
 
