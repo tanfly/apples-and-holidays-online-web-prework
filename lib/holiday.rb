b26c0e136bac5238880de9f9bf76743280dc68ad
Stella McEwen <mcewen.stella@gmail.com>
2019-04-16 22:18:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,7 +17,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  holiday_supplies[:fourth_of_july]
+  holiday_supplies.each do |
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
